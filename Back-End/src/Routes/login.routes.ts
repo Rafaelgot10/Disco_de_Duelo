a472e1f1c4602cc8f86daef3814493e8e0450948
Rafael Rocha <114513848+Rafaelgot10@users.noreply.github.com>
2025-05-27 23:20:08 -0300

    Delete Back-End directory

--- Back-End/src/Routes/login.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import loginController from "../Controllers/login.controllers";
-
-const loginRouter = Router();
-
-loginRouter.post("", loginController);
-
-export default loginRouter;
