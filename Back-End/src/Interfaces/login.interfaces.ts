3d913dc1f3053662c150aec2fb07f21126bc800c
Rafael Rocha <114513848+Rafaelgot10@users.noreply.github.com>
2025-06-03 21:57:39 -0300

    Delete Back-End directory

--- Back-End/src/Interfaces/login.interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import {
-  responseLoginSchema,
-  resquestLoginSchema,
-} from "../Schemas/login.schemas";
-
-export type TLoginRequest = z.infer<typeof resquestLoginSchema>;
-
-export type TLoginResponse = z.infer<typeof responseLoginSchema>;
